5ff050b1cf2f3af85c13405ba492b903bb0c2cd3
Timmo <40892102+Timmo2022@users.noreply.github.com>
2022-02-02 13:16:19 +0300

    Update instaquery.py

--- instaquery.py
+++ instaquery.py
@@ -1,8 +1,9 @@
-query = str(input("Do you write code?"))
-
-if query.lower() in ['yes', 'yeah', 'definitely']:
-    print(" I Succesful joined your friends list!!")
+query = str(input("Do you write code? "))
+query = query.strip() # to ignore any white spaces created by the user. 
+query = query.split()[0] # to read only the first word.
+if query.lower() in ['yes', 'yeah', 'definitely']: # to convert the inputs to lowercase
+    name = str(input(" Hi, My name is Timmo and I Succesful joined your friends list!! \n What's your name? "))
+    print("It was a pleasure meeting you "+ name)
 else:
     print(" Timmo left the chat!")
     quit()
-    
